517620f5c17ee152c9eb4011214ee79d6ea6cf97
Tasneem Osama <tasneemosama062@gmail.com>
2026-02-22 21:05:31 +0200

    chore: adding new commands in txt file

--- src/main/java/com/swaglabs/drivers/AbstractDriver.java
+++ src/main/java/com/swaglabs/drivers/AbstractDriver.java
@@ -3,6 +3,7 @@ package com.swaglabs.drivers;
 import org.openqa.selenium.WebDriver;
 
 public abstract class AbstractDriver {
+    //abstract method to start the webDriver
     //This acts as a template/parent. It has a method called startDriver that ensures every factory follows the same structure
     public abstract WebDriver startDriver();
 }
